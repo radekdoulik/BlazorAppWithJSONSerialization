314b22bca1c356e3cf78e1c0b18d9adff7c5c490
Radek Doulik <radekdoulik@gmail.com>
2021-09-23 22:04:58 +0200

    Try to use JsonSerializable attribute

---
+++ BlazorAppWithJSONSerialization/WeatherForecast.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAppWithJSONSerialization
+{
+    internal partial class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    [JsonSerializable(typeof(WeatherForecast[]))]
+    internal partial class WeatherForecastJSContext : JsonSerializerContext
+    {
+    }
+}
