5f5658620045864a53293ce865f6a914cd20014e
Radek Doulik <radekdoulik@gmail.com>
2021-09-24 23:00:54 +0200

    Try sealed record ToString override
    And constant interpolated string

--- BlazorAppWithJSONSerialization/Program.cs
+++ BlazorAppWithJSONSerialization/Program.cs
@@ -1,11 +1,30 @@
 global using BlazorAppWithJSONSerialization;
+using Plants;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+const string message = $"{nameof(BlazorAppWithJSONSerialization)} started";
+
+Console.WriteLine(message);
+
+var tree = new Plant("Myrtaceae", "Eucalyptus", "Amygdalina");
+Console.WriteLine(tree);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+namespace Plants
+{
+    public record Plant(string Family, string Genus, string Specie)
+    {
+        public override sealed string ToString()
+        {
+            return $"F:{Family} G:{Genus} S:{Specie}";
+        }
+    }
+}
