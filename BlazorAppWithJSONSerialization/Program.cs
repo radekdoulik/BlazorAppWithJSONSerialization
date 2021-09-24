dffbb93b5ac22dfab0b676281a7b8829a759636e
Radek Doulik <radekdoulik@gmail.com>
2021-09-24 23:09:24 +0200

    Try assignment and declaration
    In the same deconstruction

--- BlazorAppWithJSONSerialization/Program.cs
+++ BlazorAppWithJSONSerialization/Program.cs
@@ -4,12 +4,22 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-const string message = $"{nameof(BlazorAppWithJSONSerialization)} started";
 
+// try constant interpolated string
+const string message = $"{nameof(BlazorAppWithJSONSerialization)} started";
 Console.WriteLine(message);
 
+// try record with sealed ToString()
 var tree = new Plant("Myrtaceae", "Eucalyptus", "Amygdalina");
 Console.WriteLine(tree);
+
+// try assignment and declaration in same deconstruction
+(double x, double y, double z) point;
+point = (1.0, 0, 0);
+
+double x1;
+(x1, double y1, var z1) = point;
+Console.WriteLine($"3D point: {x1}, {y1}, {z1} == {point}");
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
